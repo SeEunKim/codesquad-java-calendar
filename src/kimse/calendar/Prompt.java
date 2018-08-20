4be60d145bafc812a03452d17067ca5b751eab6f
김세은 <kimse9450@gmail.com>
2018-08-21 01:34:46 +0900

    add function weekday

--- src/kimse/calendar/Prompt.java
+++ src/kimse/calendar/Prompt.java
@@ -3,39 +3,72 @@ package kimse.calendar;
 import java.util.Scanner;
 
 public class Prompt {
-	
-	private final static String PROMPT = "cal> ";
-	
+
+	/**
+	 * 
+	 * @param week 요일
+	 * @return 0 ~ 6 ( 0 = Sunday, 7 = Saturday)
+	 */
+	public int parseDay(String week) {
+		if (week.equals("SU")) {
+			return 0;
+		} else if (week.equals("MO")) {
+			return 1;
+		} else if (week.equals("TU")) {
+			return 2;
+		} else if (week.equals("WE")) {
+			return 3;
+		} else if (week.equals("TH")) {
+			return 4;
+		} else if (week.equals("FR")) {
+			return 5;
+		} else if (week.equals("SA")) {
+			return 6;
+		} else {
+			return 0;
+		}
+
+	}
+
 	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		
+
 		int month = 1;
 		int year = 2018;
-		
+		int weekday = 0;
+
 		while (true) {
-			System.out.println("년도를 입력하세요");
+			System.out.println("년도를 입력하세요. (exit : -1) ");
 			System.out.println("YEAR > ");
 			year = scanner.nextInt();
-			System.out.println("달을 입력하세요");
+			if(year == -1) {
+				break;
+			}
+
+			System.out.println("달을 입력하세요.");
 			System.out.println("MONTH > ");
 			month = scanner.nextInt();
 			
-			if (month == -1) {
-				break;
-			} 
-			
-			if(month > 12) {
+			if (month > 12 || month < 1) {
+				System.out.println("잘못된 입력입니다. ");
 				continue;
 			}
-			cal.printCalendar(year, month);
+
+			System.out.println("첫번째 요일을 입력하세요. (SU, MO, WE, TH, FR, SA");
+			System.out.println("WEEKDAY > ");
+			String str_weekday = scanner.next();
+			weekday = parseDay(str_weekday);
+
+			
+			cal.printCalendar(year, month, weekday);
 		}
 		System.out.printf("Bye~");
-		
+
 //		cal.printSampleCalendar();
 		scanner.close();
 	}
-	
+
 	public static void main(String[] args) {
 		Prompt p = new Prompt();
 		p.runPrompt();
--- src/kimse/calendar/Sum.java
+++ src/kimse/calendar/Sum.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Sum {
 	public static void main(String[] args) {
 		int a, b;
-		
+
 		// 입력 : 키보드로 두 수의 입력을 받는다.
-		Scanner scanner = new Scanner(System.in); 
+		Scanner scanner = new Scanner(System.in);
 		String s1, s2;
 		System.out.println("두 수를 입력해주세요 ");
 		s1 = scanner.next();
@@ -15,8 +15,8 @@ public class Sum {
 		a = Integer.parseInt(s1);
 		b = Integer.parseInt(s2);
 
-		//출력 : 화면에 두 수의 합을 출력한다.
-		System.out.printf("%d와 %d의 합은 %d입니다.",a, b, a+b );
+		// 출력 : 화면에 두 수의 합을 출력한다.
+		System.out.printf("%d와 %d의 합은 %d입니다.", a, b, a + b);
 		scanner.close();
 	}
 }
