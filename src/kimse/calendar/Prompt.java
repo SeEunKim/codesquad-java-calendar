2b94239ced1f7229a1dd232124eab44d33625873
김세은 <kimse9450@gmail.com>
2018-08-21 00:07:30 +0900

    add Prompt Class

---
+++ src/kimse/calendar/Prompt.java
@@ -0,0 +1,39 @@
+package kimse.calendar;
+
+import java.util.Scanner;
+
+public class Prompt {
+	
+	private final static String PROMPT = "cal> ";
+	
+	public void runPrompt() {
+		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
+		
+		int month = 1;
+		
+		while (true) {
+			System.out.println("월을 입력하세요");
+			System.out.println(PROMPT);
+			month = scanner.nextInt();
+			
+			if (month == -1) {
+				break;
+			} 
+			
+			if(month > 12) {
+				continue;
+			}
+			cal.printCalendar(2017, month);
+		}
+		System.out.printf("Bye~");
+		
+//		cal.printSampleCalendar();
+		scanner.close();
+	}
+	
+	public static void main(String[] args) {
+		Prompt p = new Prompt();
+		p.runPrompt();
+	}
+}
